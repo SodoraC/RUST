f82fb4c3657ab3c95ae02296c38574864b4ab063
titancupid <141187361+titancupid@users.noreply.github.com>
2026-03-03 16:03:44 -0500

    new feature

--- hello.rs
+++ hello.rs
@@ -1,4 +1,19 @@
-fn main() {
+use std::io::{self, Write};
 
-    println!("Hello World!");
+fn get_user_name() -> String {
+    let mut name = String::new();
+
+    print!("Please enter your name: ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+        name.trim().to_string()
+}
+
+fn main() {
+    let name = get_user_name();
+    println!("Hello, {}!", name);
 }
